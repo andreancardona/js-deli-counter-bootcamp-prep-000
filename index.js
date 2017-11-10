b3479773b95ca6551ddd4ee8ba373dfd9be9075b
andreancardona@gmail.com <andreancardona@gmail.com>
2017-11-10 16:04:05 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,16 @@
+var takeANumber = function (katzDeli, newCustomer) {
+  for ( var i = 0; i < katzDeli.length; i++) {
+    katzDeli.push(newPerson);
+     return `Welcome, ${newPerson}. You are number ${katzDeli.length + 1} in line.`
+   }
+}
+
+function nowServing (katzDeliLine, newCustomer) {
+   if(katzDeliLine.length===0) {
+   return 'There is nobody waiting to be served!';
+}
+   for (var i=0; i < katzDeliLine.length; i++) {
+   var removePerson = katzDeliLine.shift();
+}
+   return "Currently serving" + katzDeliLine[0] + ".";
+}
